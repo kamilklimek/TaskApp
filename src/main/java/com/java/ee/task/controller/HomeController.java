414c7c3a6361b83758763e3eeaec2e8e929ab468
klimson <klimson@gitlab.com>
2019-02-01 00:41:28 +0100

    Welcome message

--- src/main/java/com/java/ee/task/controller/HomeController.java
+++ src/main/java/com/java/ee/task/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.java.ee.task.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.HttpRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @RequestMapping("/")
@@ -11,7 +16,13 @@ import java.security.Principal;
 @Slf4j
 public class HomeController {
     @RequestMapping(value = "")
-    public String getHomeView(Principal principal) {
+    public String getHomeView(Model model, Principal principal) {
+        if (principal == null) {
+            model.addAttribute("logged", false);
+        } else {
+            model.addAttribute("login", principal.getName());
+            model.addAttribute("logged", true);
+        }
         return "home";
     }
 }
